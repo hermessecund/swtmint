2152f20ffa6e8df7bd455f6427990ed6e4ce231c
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-08-10 22:55:11 +0100

    Update Simple_Token.sol

--- src/Contracts/Simple_Token.sol
+++ src/Contracts/Simple_Token.sol
@@ -1,15 +1,47 @@
-// contracts/GLDToken.sol
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// File: contracts/binutecoin.sol
 
-contract Simple_Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Simple_Token", "ST") {
-        _mint(msg.sender, initialSupply);
+pragma solidity ^0.8.2;
+
+contract SWT6 {
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+    uint public totalSupply = 500000000 * 10 ** 18;
+    string public name = "SWT6";
+    string public symbol = "SWT6";
+    uint public decimals = 18;
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
     }
-
-    function decimals() public pure override returns (uint8) {
-        return 2;
+    
+    function balanceOf(address owner) public returns(uint) {
+        return balances[owner];
+    }
+    
+    function transfer(address to, uint value) public returns(bool) {
+        require(balanceOf(msg.sender) >= value, 'balance too low');
+        balances[to] += value;
+        balances[msg.sender] -= value;
+       emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint value) public returns(bool) {
+        require(balanceOf(from) >= value, 'balance too low');
+        require(allowance[from][msg.sender] >= value, 'allowance too low');
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;   
+    }
+    
+    function approve(address spender, uint value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;   
     }
 }
